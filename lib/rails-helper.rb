d752143efec75b4bfeae0db33b9ccc9a80719b3a
John Maxwell <jgwmaxwell@gmail.com>
2012-01-16 16:57:51 +0000

    pushed removal of unneccessary code

--- lib/rails-helper.rb
+++ lib/rails-helper.rb
@@ -1,9 +1,4 @@
 require 'action_view/helpers'
 ActionView::Helpers::AssetTagHelper.module_eval do
   include Cambelt::Helpers::Placeholder
-  #def placeholder_image_tag(cambelt_opts={}, image_opts={})
-  #  string = Cambelt.placeholder(cambelt_opts)
-  #  image_tag(string, image_opts)
-  #end
-  #alias_method :placeholder, :placeholder_image_tag
 end
