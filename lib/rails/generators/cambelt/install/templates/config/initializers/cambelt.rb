4ca89fc049212d86283eb88eae48fafa1ecf4f34
John Maxwell <jgwmaxwell@gmail.com>
2012-01-16 16:02:29 +0000

    initializer generator

---
+++ lib/rails/generators/cambelt/install/templates/config/initializers/cambelt.rb
@@ -0,0 +1,16 @@
+#
+# The initializer file for Cambelt - the Image Placeholder service.
+# Currently, all of the settings below are the defaults, to change
+# a default, then please uncomment the top and bottom lines, and then
+# uncomment and change any of the other lines you wish.
+#
+
+#Cambelt.configure do |config|  
+#   width = 640
+#   height = 360
+#   font = "Questrial"
+#   font_size = 48
+#   text = "Sample Text"
+#   bg_color = "#cccccc"
+#   fg_color = "#333333"
+#end
