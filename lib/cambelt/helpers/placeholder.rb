b44abfaca70fb9c50f8a37f37bc6b98f82bea861
John Maxwell <jgwmaxwell@gmail.com>
2012-01-16 14:12:18 +0000

    trying to fix actionview

--- lib/cambelt/helpers/placeholder.rb
+++ lib/cambelt/helpers/placeholder.rb
@@ -1,17 +1,19 @@
 module Cambelt
   module Helpers
     module Placeholder
-      def placeholder_image_tag(cambelt_opts={}, image_opts={})
-        string = Cambelt.placeholder(cambelt_opts)
-        image_tag(string, image_opts)
-      end
+      class << self
+        def placeholder_image_tag(cambelt_opts={}, image_opts={})
+          string = Cambelt.placeholder(cambelt_opts)
+          image_tag(string, image_opts)
+        end
   
-      private
-      def image_tag(link, options)
-        string = "<img src=\"#{link}\" "
-        options.each{ |key, val| string << "#{key}=\"#{val}\"" }
-        string << ' />'
-        string
+        private
+        def image_tag(link, options)
+          string = "<img src=\"#{link}\" "
+          options.each{ |key, val| string << "#{key}=\"#{val}\"" }
+          string << ' />'
+          string
+        end
       end
     end
   end
