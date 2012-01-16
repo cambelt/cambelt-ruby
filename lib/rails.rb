c7a83887c65b924157fd0bb9bcb2afc82734a93b
John Maxwell <jgwmaxwell@gmail.com>
2012-01-16 13:50:19 +0000

    trying to fix actionview

---
+++ lib/rails.rb
@@ -0,0 +1,7 @@
+require 'action_view'
+ActionView::Helpers::AssetTagHelper.module_eval do
+  def placeholder_image_tag(cambelt_opts={}, image_opts={})
+    string = Cambelt.placeholder(cambelt_opts)
+    image_tag(string, image_opts)
+  end
+end
