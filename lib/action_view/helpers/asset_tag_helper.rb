3fd38dcb46bb9a2a9644c49fad080f5672a3780b
John Maxwell <jgwmaxwell@gmail.com>
2012-01-16 13:32:09 +0000

    trying to fix actionview

--- lib/action_view/helpers/asset_tag_helper.rb
+++ lib/action_view/helpers/asset_tag_helper.rb
@@ -1,4 +1,4 @@
-module ActionView::Helpers::AssetTagHelper
+module ActionView::Helpers::AssetTagHelper.module_eval do
   def placeholder_image_tag(cambelt_opts={}, image_opts={})
     string = Cambelt.placeholder(cambelt_opts)
     image_tag(string, image_opts)
