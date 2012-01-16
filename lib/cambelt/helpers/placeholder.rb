94782d317b03b0b4f57339db874d535927f8f2be
John Maxwell <jgwmaxwell@gmail.com>
2012-01-16 14:51:02 +0000

    trying to html safe

--- lib/cambelt/helpers/placeholder.rb
+++ lib/cambelt/helpers/placeholder.rb
@@ -3,14 +3,18 @@ module Cambelt
     module Placeholder
         def placeholder_image_tag(cambelt_opts={}, image_opts={})
           string = Cambelt.placeholder(cambelt_opts)
-          image_tag(string, image_opts)
+          if defined? Rails
+            image_tag(string, image_opts).html_safe
+          else
+            image_tag(string, image_opts)
+          end
         end
         alias_method :placeholder, :placeholder_image_tag
         
         private
         def image_tag(link, options)
           string = "<img src=\"#{link}\" "
-          options.each{ |key, val| string << "#{key}=\"#{val}\"" }
+          options.each{ |key, val| string << "#{key}=\"#{val}\" " }
           string << ' />'
           string
         end
